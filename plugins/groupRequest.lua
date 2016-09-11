9fd970296f8246afac8c488d488f04b76022333f
akambagroself <mahor.akam20@yahoo.com>
2016-09-11 22:38:42 +0430

    Update groupRequest.lua

--- plugins/groupRequest.lua
+++ plugins/groupRequest.lua
@@ -1,13 +1,13 @@
 do
 
  function run(msg, matches)
- local ch = '1040275374'..msg.to.id
+ local ch = '170788440'..msg.to.id
  local fuse = 'New SuperGroup Request!\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username ..'\n\nMessage From: '..msg.to.id.. '\n\nThe Pm:\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
    local text = matches[1]
-   local chat = "chat#id"..1040275374
+   local chat = "chat#id"..170788440
 
   local sends = send_msg(chat, fuse, ok_cb, false)
   return 'Request Is Sent!\nYour SuperGroup Is Waiting For Create!'
@@ -25,4 +25,4 @@ do
   },
   run = run
  }
---by @MehdiHS
+--by @NinjaHacker
