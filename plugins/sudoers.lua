a8a451d78e257bae7e6584fe27673778b5a8f3c0
akambagroself <mahor.akam20@yahoo.com>
2016-09-11 23:50:11 +0430

    Update sudoers.lua

--- plugins/sudoers.lua
+++ plugins/sudoers.lua
@@ -4,9 +4,9 @@ function run(msg, matches)
   return [[
 #Sudoers!
   
-  1.@MehdiHS🌐 #Developer ( use #adddeveloper command to add #Mehdi_HS in your Groups & SuperGroups. )
+  1.@NinjaHacker🌐 #Developer ( use #adddeveloper command to add #NinjaHacker in your Groups & SuperGroups. )
   
-  2.@YflQw🌐 #Manager ( use #addmanager command to add #Vandad_Yflqw in your Groups & SuperGroups. )
+  2.@NinjaHacker🌐 #Manager ( use #addmanager command to add #NinjaHacker in your Groups & SuperGroups. )
   
 ]]
 end
