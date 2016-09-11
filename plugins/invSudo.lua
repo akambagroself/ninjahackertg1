1d2c2dd4acc4003a00e1c56273144d7bbdf353ee
akambagroself <mahor.akam20@yahoo.com>
2016-09-11 23:28:28 +0430

    Update invSudo.lua

--- plugins/invSudo.lua
+++ plugins/invSudo.lua
@@ -1,5 +1,5 @@
---Created by @MehdiHS
---Telegram.me/MehdiHS
+--Created by @NinjaHacker
+--Telegram.me/Ninjahacker
 do
 
 local function callback(extra, success, result)
@@ -10,13 +10,13 @@ end
 local function run(msg, matches)
  if matches[1] == 'adddeveloper' then
         chat = 'chat#'..msg.to.id
-        user1 = 'user#'..56693692
+        user1 = 'user#'..118208388
         chat_add_user(chat, user1, callback, false)
 	return "Adding Bot develper..."
       end
 if matches[1] == 'addmanager' then
         chat = 'chat#'..msg.to.id
-        user2 = 'user#'..126388065
+        user2 = 'user#'..118208388
         chat_add_user(chat, user2, callback, false)
 	return "Adding Bot manager..."
       end
